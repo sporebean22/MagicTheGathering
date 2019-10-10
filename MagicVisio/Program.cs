095fbc82bc922b504137908b1596d071170ff3a0
Stphen Jukes <dukies_2000@yahoo.com>
2019-10-10 18:22:35 +0100

    Add project files.

---
+++ MagicVisio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagicVisio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
